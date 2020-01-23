0ca184748b4b69c961a4eaa1b253a55e6884e97b
vauxhall <zero.griffin@gmail.com>
2020-01-23 13:31:33 +0100

    Update sprite

--- mac/Scene.swift
+++ mac/Scene.swift
@@ -3,9 +3,3 @@ import GameplayKit
 protocol Scene {
     var grid: GKGridGraph<GKGridGraphNode> { get }
 }
-
-extension Scene {
-    func point(_ position: vector_int2) -> CGPoint {
-        .init(x: .init(position.x) * 32, y: .init(position.y) * 32)
-    }
-}
--- mac/Sprite.swift
+++ mac/Sprite.swift
@@ -1,10 +1,12 @@
 import GameplayKit
 
 final class Sprite: GKComponent {
+    private static let size = CGFloat(32)
+    
     var position = vector_int2.zero {
         didSet {
-            node.run(.move(to: (node.scene as! Scene).point(position), duration: 0.2), withKey: "move")
+            node.run(.move(to: .init(x: .init(position.x) * Sprite.size, y: .init(position.y) * Sprite.size), duration: 0.2), withKey: "move")
         }
     }
-    let node = SKSpriteNode(texture: nil, size: .init(width: 32, height: 32))
+    let node = SKSpriteNode(texture: nil, size: .init(width: Sprite.size, height: Sprite.size))
 }
