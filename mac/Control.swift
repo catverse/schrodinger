ba402118d4f7321693b402c63b08c4bbfe529a35
vauxhall <zero.griffin@gmail.com>
2020-01-23 09:45:38 +0100

    Refactor state machine

--- mac/Control.swift
+++ mac/Control.swift
@@ -8,44 +8,19 @@ final class Control: GKComponent {
     
     override func didAddToEntity() {
         let node = (entity as! Player).component(ofType: Sprite.self)!.node
-        state = GKStateMachine(states: [Front(node), Left(node), LeftA(node), LeftB(node), Right(node), RightA(node), RightB(node)])
-        state.enter(Front.self)
+        state = GKStateMachine(states: [
+            Front0(node), Front1(node), Front2(node),
+            Back0(node), Back1(node), Back2(node),
+            Left0(node), Left1(node), Left2(node),
+            Right0(node), Right1(node), Right2(node)])
+        state.enter(Front0.self)
     }
     
     override func update(deltaTime: TimeInterval) {
         timer -= deltaTime
         if timer <= 0 {
             timer = 0.25
-            switch direction {
-            case .down: state.enter(Front.self)
-            case .left:
-                switch state.currentState {
-                case is Left, is LeftB:
-                    state.enter(LeftA.self)
-                case is LeftA:
-                    state.enter(LeftB.self)
-                default:
-                    state.enter(Left.self)
-                }
-            case .right:
-                switch state.currentState {
-                case is Right, is RightB:
-                    state.enter(RightA.self)
-                case is RightA:
-                    state.enter(RightB.self)
-                default:
-                    state.enter(Right.self)
-                }
-            case .none:
-                switch state.currentState {
-                case is LeftA, is LeftB:
-                    state.enter(Left.self)
-                case is RightA, is RightB:
-                    state.enter(Right.self)
-                default: break
-                }
-            default: break
-            }
+            (state.currentState as! Stand).direction(direction)
             direction = .none
         }
     }
--- mac/Stand.swift
+++ mac/Stand.swift
@@ -1,7 +1,10 @@
 import GameplayKit
 
 class Stand: GKState {
-    fileprivate var texture: String { "" }
+    fileprivate var texture: String! { nil }
+    fileprivate var compare: Key! { nil }
+    fileprivate var next: AnyClass! { nil }
+    fileprivate var fallback: AnyClass! { nil }
     private weak var node: SKSpriteNode!
     
     init(_ node: SKSpriteNode) {
@@ -9,39 +12,101 @@ class Stand: GKState {
         self.node = node
     }
     
-    override func didEnter(from: GKState?) {
+    final override func didEnter(from: GKState?) {
         node.run(.setTexture(.init(imageNamed: texture)))
+    }
+    
+    final func direction(_ direction: Key) {
+        if direction == compare {
+            stateMachine!.enter(next)
+        } else {
+            switch direction {
+            case .up: stateMachine!.enter(Back0.self)
+            case .down: stateMachine!.enter(Front0.self)
+            case .left: stateMachine!.enter(Left0.self)
+            case .right: stateMachine!.enter(Right0.self)
+            default: stateMachine!.enter(fallback)
+            }
+        }
     }
 }
 
-final class Front: Stand {
+class Front: Stand {
+    override var compare: Key { .down }
+    override var fallback: AnyClass { Front0.self }
+}
+
+class Back: Stand {
+    override var compare: Key { .up }
+    override var fallback: AnyClass { Back0.self }
+}
+
+class Left: Stand {
+    override var compare: Key { .left }
+    override var fallback: AnyClass { Left0.self }
+}
+
+class Right: Stand {
+    override var compare: Key { .right }
+    override var fallback: AnyClass { Right0.self }
+}
+
+final class Front0: Front {
     override var texture: String { "front" }
+    override var next: AnyClass { Front1.self }
 }
 
-final class Back: Stand {
+final class Front1: Front {
+    override var texture: String { "front_walk_a" }
+    override var next: AnyClass { Front2.self }
+}
+
+final class Front2: Front {
+    override var texture: String { "front_walk_b" }
+    override var next: AnyClass { Front1.self }
+}
+
+final class Back0: Back {
     override var texture: String { "back" }
+    override var next: AnyClass { Back1.self }
 }
 
-final class Left: Stand {
+final class Back1: Back {
+    override var texture: String { "back_walk_a" }
+    override var next: AnyClass { Back2.self }
+}
+
+final class Back2: Back {
+    override var texture: String { "back_walk_b" }
+    override var next: AnyClass { Back1.self }
+}
+
+final class Left0: Left {
     override var texture: String { "left" }
+    override var next: AnyClass { Left1.self }
 }
 
-final class LeftA: Stand {
+final class Left1: Left {
     override var texture: String { "left_walk_a" }
+    override var next: AnyClass { Left2.self }
 }
 
-final class LeftB: Stand {
+final class Left2: Left {
     override var texture: String { "left_walk_b" }
+    override var next: AnyClass { Left1.self }
 }
 
-final class Right: Stand {
+final class Right0: Right {
     override var texture: String { "right" }
+    override var next: AnyClass { Right1.self }
 }
 
-final class RightA: Stand {
+final class Right1: Right {
     override var texture: String { "right_walk_a" }
+    override var next: AnyClass { Right2.self }
 }
 
-final class RightB: Stand {
+final class Right2: Right {
     override var texture: String { "right_walk_b" }
+    override var next: AnyClass { Right1.self }
 }
